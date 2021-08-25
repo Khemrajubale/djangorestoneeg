6f4814e71974812b081079b01aed94250a90bc78
khemrajubale <ubalekhemrajmit@gmail.com>
2021-08-25 09:29:29 +0530

    test

--- languages/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Language, LanguageSalesforce
-
-class LanguageSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = LanguageSalesforce
-        fields = ('name',)
--- languages/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import LanguageSalesforce
-from .serializers import LanguageSerializer
-
-class LanguageView(viewsets.ModelViewSet):
-    queryset = LanguageSalesforce.objects.all()
-    serializer_class = LanguageSerializer
